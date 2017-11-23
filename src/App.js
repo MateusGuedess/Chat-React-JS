e8dec40cdf2ed754895367fa3dc2e4275d12c9ac
Mateus Guedes da Conceição <teuguedes@outlook.com>
2017-11-22 22:59:32 -0200

    commit inicial do projeto

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import 'bootstrap-css-only'
+
+import NewComment from './NewComment'
+import Comments from './Comments'
+
+class App extends Component {
+  constructor(props){
+    super(props)
+
+    this.postNewComment = this.postNewComment.bind(this)
+
+    this.state = {
+      comments: {
+        '1' : {
+          comment: 'first comment'
+        },
+        '2' : {
+          comment: 'Second Comment'
+        }
+      }
+    }
+    
+  }
+
+  postNewComment(comment){
+    this.setState({
+      comments: {
+        ...this.state.comments, comment
+      }
+    })
+  }
+  render() {
+    return (
+      <div className="container">
+        <NewComment postNewComment={this.postNewComment} />
+        <Comments comments={this.state.comments} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/NewComment.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+
+class NewComment extends Component {
+    constructor(props){
+        super(props)
+
+        this.handleEnter = this.handleEnter.bind(this)
+    }
+    handleEnter(event){
+        if(event.keyCode === 13){
+            this.props.postNewComment({
+                comment:'test'
+            })
+            event.preventDefault()
+               
+        }
+    }
+    render(){
+        return(
+            <div className="row">
+                <textarea placeholder="Comente!" className="form-control" onKeyDown={this.handleEnter}></textarea>
+            </div>
+        )
+    }
+}
+
+export default NewComment
